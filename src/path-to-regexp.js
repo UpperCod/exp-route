07a36de27aac66c6cf53cfab74866140013f217c
UpperCod <mtrujillo@outlook.cl>
2021-03-26 19:54:41 -0300

    fix hash usage

--- src/path-to-regexp.js
+++ src/path-to-regexp.js
@@ -5,23 +5,30 @@
  */
 export function pathToRegExp(path) {
     const params = [];
-    const regFolders = path.split(/\//g).map((folder) => {
-        if (!folder) return "";
-        const [, hash = "", type, spread, param] = folder.match(
-            /(#){0,1}([\{\[]){0,1}(\.\.\.){0,1}(\w+)([\}\]]){0,1}/
-        );
-        if (type == "{" || type == "[") {
-            params.push(param);
-            const wildcard = type == "[" ? "*" : "+";
-            const slash =
-                (hash ? "(?:/#|#)" : "(?:/)") + (type == "[" ? "{0,1}" : "{1}");
-            return spread
-                ? `${slash}(.${wildcard})`
-                : `${slash}([^\/#]${wildcard})`;
-        } else {
-            return (folder[0] == "#" ? "(?:/#|#)" : "/") + ignore(folder);
-        }
-    });
+    const regFolders = path
+        .replace(/#(\/){0,1}/, "/#")
+        .split(/\//g)
+        .map((folder) => {
+            if (!folder) return "";
+            const [, hash = "", type, spread, param] = folder.match(
+                /(#){0,1}([\{\[]){0,1}(\.\.\.){0,1}(\w*)([\}\]]){0,1}/
+            );
+            if (type == "{" || type == "[") {
+                params.push(param);
+                const wildcard = type == "[" ? "*" : "+";
+                const slash =
+                    (hash ? "(?:/#|#)" : "(?:/)") +
+                    (type == "[" ? "{0,1}" : "{1}");
+                return spread
+                    ? `${slash}(.${wildcard})`
+                    : `${slash}([^\/#]${wildcard})`;
+            } else {
+                return (
+                    (hash ? "(?:/#|#)" : "/") +
+                    ignore(hash ? folder.slice(1) : folder)
+                );
+            }
+        });
     return [RegExp("^" + (regFolders.join("") || "(?:/){0,1}") + "$"), params];
 }
 
